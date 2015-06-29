55bfa6db4ca33aa9f9e2f2113f271e00816d2f0c
Kolesár András <kolesar@kolesar.hu>
2015-06-29 08:07:00 +0200

    frissítés adatainak kijelzése json formátumban

--- update.php
+++ update.php
@@ -1,6 +1,18 @@
 <?php
 ini_set('display_errors', 1);
-passthru('./download.sh');
-echo filesize('overpass.geojson');
+$file = 'overpass.geojson';
+$data = [];
+$data['old'] = fileinfo($file);
+shell_exec('./download.sh');
+clearstatcache();
+$data['new'] = fileinfo($file);
 
+header('Content-type: text/plain; charset=utf-8');
+echo json_encode($data, JSON_PRETTY_PRINT);
 
+function fileinfo ($file) {
+    return [
+        'size' => filesize($file),
+        'time' => date('Y-m-d H:i:s', filemtime($file)),
+    ];
+}
