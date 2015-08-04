bb0262fa97d36ef3e530fc13df4c6698f7f7a413
Kolesár András <kolesar@kolesar.hu>
2015-08-04 10:48:00 +0200

    kml kimenet

--- download.sh
+++ download.sh
@@ -3,3 +3,5 @@
 rm -f overpass.json
 curl -o overpass.json --data-urlencode data@overpass.ql http://overpass-api.de/api/interpreter 2>overpass.stderr
 cat overpass.json | ./json-geojson.php > overpass.geojson
+cat overpass.json | ./json-geojson-kml.php > overpass-kml.geojson
+ogr2ogr -F KML overpass.kml overpass-kml.geojson
---
+++ json-geojson-kml.php
@@ -0,0 +1,40 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * overpass json átalakítása geojson-ra
+ *
+ * @author Kolesár András <kolesar@kolesar.hu>
+ * @since 2015.03.06
+ *
+ */
+
+$json = json_decode(file_get_contents('php://stdin'), true);
+$features = array();
+
+foreach ($json['elements'] as $element) {
+	$p = [];
+	foreach ($element as $k => $v) {
+		if ($k == 'tags') {
+			$p = array_merge($p, $v);
+		} else {
+			$p['['.$k.']'] = $v;
+		}
+	}
+	$p['name'] = $p['operator'];
+	$features[] = array(
+		'type' => 'Feature',
+		'geometry' => array(
+			'type' => 'Point',
+			'coordinates' => array($element['lon'], $element['lat']),
+		),
+		'properties' => $p,
+	);
+}
+
+echo json_encode(array(
+	'type' => 'FeatureCollection',
+	'features' => $features,
+    ),
+    JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE
+);
