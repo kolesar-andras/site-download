243bf3a3e05291f94c09229db473158832574d2b
Kolesár András <kolesar@kolesar.hu>
2015-08-23 14:48:00 +0200

    llama fájl gyártásának automatizálása

--- download.sh
+++ download.sh
@@ -4,4 +4,5 @@ rm -f overpass.json
 curl -o overpass.json --data-urlencode data@overpass.ql http://overpass-api.de/api/interpreter 2>overpass.stderr
 cat overpass.json | ./json-geojson.php > overpass.geojson
 cat overpass.json | ./json-geojson-kml.php > overpass-kml.geojson
+cat overpass.json | ./json-llama.php > Llama_Areas.txt
 ogr2ogr -F KML overpass.kml overpass-kml.geojson
