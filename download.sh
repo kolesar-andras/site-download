4461b53bbb42dfc8209fe7e88f56b8afca861c9d
Kolesár András <kolesar@kolesar.hu>
2019-06-28 12:03:06 +0200

    kimeneti könyvtár kezelése

--- download.sh
+++ download.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-rm -f overpass.new.json
-curl -o overpass.new.json --data-urlencode data@overpass.ql http://overpass-api.de/api/interpreter 2>overpass.stderr
-if [ $(wc -c < overpass.new.json) -ge 10000 ]; then
-    rm -f overpass.json
-    mv overpass.new.json overpass.json
-    cat overpass.json | ./json-geojson.php > overpass.geojson
-    cat overpass.json | ./json-geojson-kml.php > overpass-kml.geojson
-    cat overpass.json | ./json-llama.php > Llama_Areas.txt
-    cat overpass.json | ./json-clf.php > overpass.clf
-    cat overpass.json | ./json-cells.php | psql kolesar > /dev/null 2>&1
-    ogr2ogr -F KML overpass.kml overpass-kml.geojson
+OUTPUT=${OUTPUT:-output}
+LOG=${LOG:-log}
+
+mkdir -p $OUTPUT
+mkdir -p $LOG
+
+rm -f $OUTPUT/overpass.new.json
+curl -o $OUTPUT/overpass.new.json --data-urlencode data@overpass.ql http://overpass-api.de/api/interpreter 2>$LOG/overpass.stderr
+if [ $(wc -c < $OUTPUT/overpass.new.json) -ge 10000 ]; then
+    rm -f $OUTPUT/overpass.json
+    mv $OUTPUT/overpass.new.json $OUTPUT/overpass.json
+    cat $OUTPUT/overpass.json | ./json-geojson.php > $OUTPUT/overpass.geojson
+    cat $OUTPUT/overpass.json | ./json-geojson-kml.php > $OUTPUT/overpass-kml.geojson
+    cat $OUTPUT/overpass.json | ./json-llama.php > $OUTPUT/Llama_Areas.txt
+    cat $OUTPUT/overpass.json | ./json-clf.php > $OUTPUT/overpass.clf
+    cat $OUTPUT/overpass.json | ./json-cells.php | psql kolesar > $LOG/psql.log 2>&1
+    ogr2ogr -F KML $OUTPUT/overpass.kml $OUTPUT/overpass-kml.geojson
 fi
